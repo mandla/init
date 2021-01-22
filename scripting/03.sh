cefd804fb65a0a2519b1bdad2f4147de3cf9f6bb
mashinini <54777680+mandla@users.noreply.github.com>
2021-01-22 10:13:31 +0200

    Update 03.sh

--- scripting/03.sh
+++ scripting/03.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-echo "That's it for init"
+echo "Enter username"
+read username
+echo "Enter password"
+read password
+
+if [[ ( $username == "admin" && $password == "secret" ) ]]; then
+echo "valid user"
+else
+echo "invalid user"
+fi
